505be7b38671b9b3c861d453058ec9c0449a094c
AntoinePr <antoine.protard@gmail.com>
2017-04-27 13:52:53 +0200

    Big commit, add of several librairies

---
+++ Projet_SR03/src/controller/Example.java
@@ -0,0 +1,7 @@
+package controller;
+
+public class Example {
+	public int add(int a, int b){
+		return a+b;
+	}
+}
---
+++ Projet_SR03/src/controller/VideogameRESTService.java
@@ -0,0 +1,23 @@
+package controller;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.Path;
+
+@Path("/msg")
+public class VideogameRESTService {
+	
+	@GET
+	public Response getUser() {
+		return Response.status(200).entity("getUser is called").build();
+	}
+
+	@GET
+	@Path("/test")
+	@Produces(MediaType.TEXT_PLAIN)
+    public String getMessage() {
+        return "Hello ";
+    }
+}
